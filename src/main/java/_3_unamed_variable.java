defa70cd73fda6a95e863046124c02e780e3f422
forax <forax@univ-mlv.fr>
2024-06-17 11:45:58 +0200

    improve unnamed variable example

--- src/main/java/_3_unamed_variable.java
+++ src/main/java/_3_unamed_variable.java
@@ -1,13 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.stream.IntStream;
 //import module java.base;
 
 void main() {
-  var list = IntStream.range(0, 5).boxed().toList();
-  //for(var element : list) {
-  //  System.out.println("hello");
-  //}
-
+  var list = IntStream.range(0, 3).boxed().toList();
   for(var _ : list) {
     System.out.println("hello");
   }
+
+  boolean exist;
+  try(var _ = Files.newInputStream(Path.of("README.md"))) {
+    exist = true;
+  } catch (IOException _) {
+    exist = false;
+  }
+  System.out.println("README exists " + exist);
 }
