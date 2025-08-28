1dbf8ddae36ad26343f7c91e24721419f9999380
forax <forax@univ-mlv.fr>
2025-08-28 10:57:41 +0200

    move open meteo api in its own package

---
+++ src/main/java/openmeteo/OpenMeteo.java
@@ -0,0 +1,62 @@
+package openmeteo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import module java.base;
+import module java.net.http;
+//import module com.fasterxml.jackson.databind;
+
+public final class OpenMeteo {
+  /**
+   * Open Meteo API response structure.
+   * see <a href="https://api.open-meteo.com/v1/forecast?latitude=48.864716&longitude=2.349014&current=temperature_2m">open meteo api</a>
+   */
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public record WeatherResponse(
+      double latitude,
+      double longitude,
+      CurrentWeather current) {
+  }
+
+  /**
+   * Current weather data from the API response.
+   */
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public record CurrentWeather(@JsonProperty("temperature_2m") double temperature) {
+  }
+
+  static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  public record LatLong(double latitude, double longitude) {
+  }
+
+  public static WeatherResponse getWeatherResponse(LatLong latLong)
+      throws IOException, InterruptedException {
+
+    var uri = URI.create("""
+        https://api.open-meteo.com/v1/forecast?latitude=%s&longitude=%s&current=temperature_2m\
+        """.formatted("" + latLong.latitude, "" + latLong.longitude));
+    IO.println("Requesting weather data from " + uri);
+
+    var request = HttpRequest.newBuilder().uri(uri).GET().build();
+
+    try (var httpClient = HttpClient.newBuilder().build()) {
+      var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+      if (response.statusCode() != 200) {
+        throw new IOException("API request failed with status: " + response.statusCode());
+      }
+      return OBJECT_MAPPER.readValue(response.body(), WeatherResponse.class);
+    }
+  }
+
+  static void main() throws IOException, InterruptedException {
+    var paris = new LatLong(48.864716, 2.349014);
+
+    var weather = getWeatherResponse(paris);
+
+    IO.println("Lat,Long " + weather.latitude + ", " + weather.longitude);
+    IO.println("Temperature " + weather.current().temperature() + "°C");
+  }
+}
